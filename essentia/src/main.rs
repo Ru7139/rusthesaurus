b4d936ccfe947553ac07494b4ae3bbdc5ae6702e
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-11-12 23:16:08 +0900

    init files

--- essentia/src/main.rs
+++ essentia/src/main.rs
@@ -1,5 +1,10 @@
+#[cfg(test)]
 mod test_of_those;
 
 fn main() {
-    println!("Hello, world!");
+    println!("right now everything is in the mod of test_of_those");
+    println!("Deciaml       1024 = {}   (10)", 1024);
+    println!("Binary        1024 = {:b} (2)", 1024);
+    println!("Octal         1024 = {:o} (8)", 1024);
+    println!("Hexadeciaml   1024 = {:x} (16)", 1024);
 }
