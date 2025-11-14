e254de22380f2993727d170f6a739d01ff68b523
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-11-14 23:33:19 +0900

    Update main.rs

--- essentia/src/main.rs
+++ essentia/src/main.rs
@@ -1,10 +1,4 @@
 #[cfg(test)]
 mod test_of_those;
 
-fn main() {
-    println!("right now everything is in the mod of test_of_those");
-    println!("Deciaml       1024 = {}   (10)", 1024);
-    println!("Binary        1024 = {:b} (2)", 1024);
-    println!("Octal         1024 = {:o} (8)", 1024);
-    println!("Hexadeciaml   1024 = {:x} (16)", 1024);
-}
+fn main() {}
