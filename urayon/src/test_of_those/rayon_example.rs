fce30fe26cdd2625807660af29611f4aa79903a8
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-11-20 07:23:40 +0900

    update

--- urayon/src/test_of_those/rayon_example.rs
+++ urayon/src/test_of_those/rayon_example.rs
@@ -7,7 +7,8 @@ static _LAZY_ARRAY: LazyLock<[u32; 1000]> =
 
 #[test]
 fn rayon_speed_test() {
-    let vec = (0..65534) // log2(u32::MAX) = 65535.999
+    // let vec = (0..65534) // log2(u32::MAX) = 65535.999 // 手动测试时使用最大值
+    let vec = (0..100) // cargo test时使用这个
         .into_iter() // 50k * 1k * 4Bytes = 200MB
         .map(|_| (1..=1000).collect::<Vec<u32>>())
         .collect::<Vec<Vec<u32>>>();
