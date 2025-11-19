f3c334b68e4c9318262d8cc0510cc7d59fd2df86
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-11-19 12:48:39 +0900

    rayon test

---
+++ urayon/src/test_of_those/rayon_example.rs
@@ -0,0 +1,45 @@
+use std::sync::LazyLock;
+
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+
+static LAZY_ARRAY: LazyLock<[u32; 1000]> =
+    LazyLock::new(|| std::array::from_fn(|x| (x + 1) as u32));
+
+#[test]
+fn rayon_speed_test() {
+    let vec = (0..65535) // log2(u32::MAX) = 65535
+        .into_iter() // 50k * 1k * 4Bytes = 200MB
+        .map(|_| Vec::from(&*LAZY_ARRAY))
+        .collect::<Vec<Vec<u32>>>();
+
+    let timer_normal = std::time::Instant::now();
+    let _ = vec
+        .clone()
+        .into_iter()
+        .flat_map(|x| x.into_iter().map(|y| y * y))
+        .collect::<Vec<u32>>();
+    dbg!(timer_normal.elapsed()); // 1436.606ms // Debug
+
+    let timer_rayon = std::time::Instant::now();
+    let _ = vec
+        .clone()
+        .into_par_iter()
+        .flat_map(|x| x.into_par_iter().map(|y| y * y))
+        .collect::<Vec<u32>>();
+    dbg!(timer_rayon.elapsed()); // 394.405ms // Debug
+
+    let timer_rayon2 = std::time::Instant::now();
+    let _ = vec
+        .clone()
+        .into_par_iter()
+        .flat_map_iter(|x| x.into_iter().map(|x| x * x))
+        .collect::<Vec<u32>>();
+    dbg!(timer_rayon2.elapsed()); // 260.633ms // Debug
+
+    let timer_rayon3 = std::time::Instant::now();
+    let _ = vec
+        .into_par_iter()
+        .flat_map_iter(|x| x.into_iter().map(|x| x * x))
+        .collect::<Vec<u32>>();
+    dbg!(timer_rayon3.elapsed()); // 170.762ms // Debug
+}
