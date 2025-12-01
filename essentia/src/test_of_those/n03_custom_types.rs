72cc8e455f132a33844b1ec93d323c34184215a7
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-12-01 20:02:56 +0900

    done struct enum static const

--- essentia/src/test_of_those/n03_custom_types.rs
+++ essentia/src/test_of_those/n03_custom_types.rs
@@ -67,7 +67,84 @@ fn structures() {
 }
 
 #[test]
-fn enums() {}
+fn enums() {
+    enum YoodyCandyItem {
+        DarkChocolate = 114201,
+        MilkChocolate = 114202,
+        LemonPop = 136651,
+        PineappleCandy = 136631,
+    }
+    impl YoodyCandyItem {
+        fn into_string(&self) -> String {
+            match self {
+                YoodyCandyItem::DarkChocolate => "DarkChocolate".into(),
+                YoodyCandyItem::MilkChocolate => "MilkChocolate".into(),
+                YoodyCandyItem::LemonPop => "LemonPop".into(),
+                YoodyCandyItem::PineappleCandy => "PineappleCandy".into(),
+            }
+        }
+
+        fn candy_bar_code(&self) -> String {
+            match self {
+                YoodyCandyItem::DarkChocolate => "114201".into(),
+                YoodyCandyItem::MilkChocolate => "114202".into(),
+                YoodyCandyItem::LemonPop => "136651".into(),
+                YoodyCandyItem::PineappleCandy => "136631".into(),
+            }
+        }
+    }
+
+    let _ = YoodyCandyItem::DarkChocolate.candy_bar_code();
+    let _ = YoodyCandyItem::MilkChocolate.into_string();
+    let _ = YoodyCandyItem::LemonPop as i32;
+    let _ = YoodyCandyItem::PineappleCandy as u32;
+
+    enum Klist {
+        Note(u32, Box<Klist>),
+        Nil,
+    }
+    impl Klist {
+        fn new() -> Klist {
+            Klist::Nil
+        }
+
+        fn prepend(self, begin_u32: u32) -> Klist {
+            Klist::Note(begin_u32, Box::new(self))
+        }
+
+        fn len(&self) -> u64 {
+            match *self {
+                Self::Nil => 0,
+                Self::Note(_, ref tail) => 1 + tail.len(),
+            }
+        }
+
+        fn stringify(&self) -> String {
+            match *self {
+                Self::Note(head, ref tail) => {
+                    format!("{}, {}", head, tail.stringify())
+                }
+                Self::Nil => {
+                    format!("Nil")
+                }
+            }
+        }
+    }
+
+    let mut klist_a = Klist::new();
+    klist_a = klist_a.prepend(10);
+    klist_a = klist_a.prepend(15);
+    klist_a = klist_a.prepend(20);
+
+    dbg!(klist_a.len());
+    dbg!(klist_a.stringify());
+}
 
 #[test]
-fn constants() {}
+fn constants() {
+    static MESSAGE: &str = "A possibly mutable variable with 'static lifetime. The static lifetime is inferred and does not have to be specified. Accessing or modifying a mutable static variable is unsafe.";
+    const PI_SQURE: f64 = std::f64::consts::PI * std::f64::consts::PI;
+
+    dbg!(MESSAGE);
+    dbg!(PI_SQURE);
+}
